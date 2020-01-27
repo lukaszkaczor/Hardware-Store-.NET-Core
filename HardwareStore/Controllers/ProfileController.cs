f9694d6733113d3ddd9428d9d74511f6f34c2c5b
Łukasz Kaczor <lukaszk9396@gmail.com>
2020-01-27 19:37:12 +0100

    Add improvements to css and html structure

--- HardwareStore/Controllers/ProfileController.cs
+++ HardwareStore/Controllers/ProfileController.cs
@@ -1,22 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using HardwareStore.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace HardwareStore.Controllers
 {
     public class ProfileController : Controller
     {
+        private ApplicationDbContext _context;
+        private string _userId;
+        public ProfileController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return RedirectToAction("Address");
+            //return View();
         }
 
         public async Task<IActionResult> Address()
         {
+            _userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
 
-            return RedirectToAction(nameof(Address));
+            var model = await _context.Addresses.FirstOrDefaultAsync(d => d.IdentityUserId == _userId);
+
+            //return RedirectToAction(nameof(Address));
+            return View(model);
         }
     }
 }
