9a61c5b92cdd5585675acfe30857df726676bb99
Lukasz <lukaszk9396@gmail.com>
2020-08-20 09:50:24 +0200

    Populate db

---
+++ HardwareStore/UnitOfWorkRepository/Interfaces/ITagRepository.cs
@@ -0,0 +1,13 @@
+﻿using HardwareStore.Models.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HardwareStore.UnitOfWorkRepository.Interfaces
+{
+    public interface ITagRepository : IRepository<Tag>
+    {
+        List<Tag> GetTags();
+    }
+}
---
+++ HardwareStore/UnitOfWorkRepository/Repositories/TagRepository.cs
@@ -0,0 +1,26 @@
+﻿using HardwareStore.Data;
+using HardwareStore.Models.DbModels;
+using HardwareStore.UnitOfWorkRepository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HardwareStore.UnitOfWorkRepository.Repositories
+{
+    public class TagRepository : Repository<Tag>,ITagRepository
+    {
+        public TagRepository(ApplicationDbContext context) : base(context){}
+
+
+        public List<Tag> GetTags()
+        {
+            return DbContext.Tags.ToList();
+        }
+
+        public ApplicationDbContext DbContext
+        {
+            get { return Context as ApplicationDbContext; }
+        }
+    }
+}
